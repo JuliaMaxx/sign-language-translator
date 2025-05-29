2c611464f8c6d1f713a448e332c4c9fc2cae4745
Julia Max <jjuliamaxxx@gmail.com>
2025-05-29 14:38:32 +0200

    retrain the asl model, generate the report

--- app.py
+++ app.py
@@ -2,12 +2,11 @@
 from flask import Flask, request, jsonify
 from keras.models import load_model
 from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
-import os
+from sklearn.metrics import classification_report
 
 app = Flask(__name__)
-
-model = load_model("models/asl_model.h5")
 
 @app.route("/")
 def index():
